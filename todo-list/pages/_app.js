5810d0e271a7489544de41db5b50c1e2f5c16c62
Eduardo <ealvarez@carminaticonsulting.com>
2021-07-11 14:18:59 -0500

    first stage of copying logic

---
+++ todo-list/pages/_app.js
@@ -0,0 +1,11 @@
+import "tailwindcss/tailwind.css";
+import { Auth } from "@supabase/ui";
+import { supabase } from "../lib/initSupabase";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Auth.UserContextProvider supabaseClient={supabase}>
+      <Component {...pageProps} />
+    </Auth.UserContextProvider>
+  );
+}
